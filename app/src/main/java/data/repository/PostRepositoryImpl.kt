b12158fa84e260c1d61175f43e6395be47d544d3
NicolasSnchz <nicosanchezvil@gmail.com>
2026-04-19 18:22:20 -0500

    Agrega repositorio para consumir posts

---
+++ app/src/main/java/data/repository/PostRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package com.lab.retrofitlab.data.repository
+
+import com.lab.retrofitlab.data.remote.api.PostApi
+import com.lab.retrofitlab.data.remote.dto.toDomain
+import com.lab.retrofitlab.domain.model.Post
+import com.lab.retrofitlab.domain.repository.PostRepository
+
+class PostRepositoryImpl(
+    private val api: PostApi
+) : PostRepository {
+
+    override suspend fun getPosts(page: Int): Result<List<Post>> {
+        return runCatching {
+            api.getPosts(page = page).map { postDto ->
+                postDto.toDomain()
+            }
+        }
+    }
+}
